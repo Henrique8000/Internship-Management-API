70c0eb9075aa58aa775895d5efcc1d32fb8ec0ee
lubj9 <lucasbaracatprofissional@gmail.com>
2026-05-04 23:49:26 -0300

    AreaInteresse.java e Curso.java

---
+++ internshipAPI/src/main/java/com/_Company/internshipAPI/model/Curso.java
@@ -0,0 +1,20 @@
+package com._Company.internshipAPI.model;
+import lombok.*;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+
+public class Curso {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
+    @Setter
+    @NotBlank
+    @Size(max = 120)
+    private String nome;
+}
