d8c7f4aa2218e0c35353a4a20477b54e5ef463fc
Henrique <henriqueflavioguimaraes@gmail.com>
2026-05-11 21:06:11 -0300

    feat: Empresa delete and update endpoints
    new endpoints for entity Empresa and adding new status values at StatusVaga enum

--- internshipAPI/src/main/java/com/_Company/internshipAPI/controller/EmpresaController.java
+++ internshipAPI/src/main/java/com/_Company/internshipAPI/controller/EmpresaController.java
@@ -3,9 +3,11 @@ package com._Company.internshipAPI.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +35,16 @@ public class EmpresaController {
 	@PostMapping("/cadastrar")
 	public void cadastrarEmpresa(@RequestBody Empresa empresa) {
 		this.empresaService.create(empresa);
+	}
+	
+	@DeleteMapping("/deletar/{id}")
+	public void deletarEmpresa(@PathVariable Long id) {
+		empresaService.deleteById(id);
+	}
+	
+	@PutMapping("/atualizar/{id}")
+	public void atualizarEmpresa(@PathVariable Long id, @RequestBody Empresa newEmpresa) {
+		empresaService.updateEmpresa(id, newEmpresa);
 	}
 	
 }
--- internshipAPI/src/main/java/com/_Company/internshipAPI/model/entities/enums/StatusVaga.java
+++ internshipAPI/src/main/java/com/_Company/internshipAPI/model/entities/enums/StatusVaga.java
@@ -1,5 +1,8 @@
 package com._Company.internshipAPI.model.entities.enums;
 
 public enum StatusVaga {
-
+	ABERTA,
+	FECHADA,
+	EM_PROCESSO,
+	CANCELADA;
 }
